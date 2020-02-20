43c0e8f01564b9397f0bf44a1e2c74208a47f33d
Tejal-gole <58540564+Tejal-gole@users.noreply.github.com>
2020-02-20 21:28:23 +0530

    Add files via upload

---
+++ recursion_1.py
@@ -0,0 +1,20 @@
+def TOH(n, from_rod, to_rod, aux_rod):
+    if n == 1:
+        print ("Move disk 1 from rod", from_rod, "to rod", to_rod)
+        return
+    # print(n)
+    # count= count+1
+    TOH(n - 1, from_rod, aux_rod, to_rod)
+    # print(n)
+    print( "Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    # print(n)
+    TOH(n - 1, aux_rod, to_rod, from_rod)
+    # print(n)
+ 
+
+# Driver code
+# n = int(input("Enter the no. of disk"))
+TOH(3, 'A', 'C', 'B')
+# A, C, B are the name of rods
+
+# Contributed By Harshit Agrawal
